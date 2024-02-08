5ec1c7b7e21a09dabb58ed2e8a6a734d29ae6095
felipe0612 <Felipe.bp1210@gmail.com>
2024-02-08 15:34:10 -0300

    excluir.php

--- 2_Cadastro (Alunos_)/2_Cadastro (Alunos_)/apagar.php
+++ 2_Cadastro (Alunos_)/2_Cadastro (Alunos_)/apagar.php
@@ -1,2 +1,29 @@
-<!-- Digite aqui (apagar.php) -->
-<!-- 5º Arquivo a ser digitado -->
+<?php
+
+include_once "conexao.php";
+
+$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+
+if (!empty($id)) {
+    $query_usuario = "DELETE FROM usuarios WHERE id=:id";
+    $del_usuario = $conn->prepare($query_usuario);
+    $del_usuario->bindParam(':id', $id);
+
+    if ($del_usuario->execute()) {
+      
+    $query_endereco = "DELETE FROM enderecos WHERE usuario_id=:usuario_id";
+    $del_endereco = $conn->prepare($query_endereco);
+    $del_endereco->bindParam(':usuario_id', $id);
+
+    if($del_endereco->execute()){
+
+        $retorna = ['status' => true, 'msg' => "<div calss='alert alert-sucess' role='alert'>Usuario apagado com sucesso!</div>"];
+    } else {
+        $retorna = ['status' => false, 'msg' => "<div calss='alert alert-danger' role='alert'>Erro: Uusario apagado, endereço não apagado com scesso!</div>"];
+    }
+    } else {
+        $retorna = ['status' => false, 'msg' => "<div calss='alert alert-danger' role='alert'>Erro: Nenhum usuário encontrado!</div>"];
+    }
+
+    echo json_encode($retorna);
+}
